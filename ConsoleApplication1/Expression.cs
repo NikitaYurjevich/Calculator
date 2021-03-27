04293edbc678ceca180cc42ac3e23c4f9f40cd42
NikitaYurjevich <80631700+NikitaYurjevich@users.noreply.github.com>
2021-03-27 17:41:30 +0500

    commit

---
+++ ConsoleApplication1/Expression.cs
@@ -0,0 +1,91 @@
+﻿//В данном классе реализуется парсинг выражения c передачей данных в дочерние классы Operation.
+
+using System;
+
+namespace Calculator
+{
+    public class Expression
+    {
+        private string str;
+        public int index;
+
+        public Expression(string _string)
+        {
+            str = _string;
+        }
+
+        //Метод, выводящий результат
+        public double Calculation()
+        {
+            index = 0;
+            Operation answer = Parse0();
+            return answer.Calc();
+        }
+
+        //Метод для поиска того или иного символа.
+        public bool Find(char ch)
+        {
+            if (index >= str.Length) return false;
+            while (str[index] == ' ') ++index;
+
+            if (str[index] == ch)
+            {
+                index++;
+                return true;
+            }
+            return false;
+        }
+        //Следующие три метода служат для парсинга элементов выражения и соответствующих вычислений.
+        //Также, здесь реализуется расстановка приоритетов для операторов в необходимом порядке: 
+        //действия в скобках, умножение/деление, сложение/вычитание.
+        public Operation Parse0()
+        {
+            Operation result = Parse1();
+
+            while (true)
+            {
+                if (Find('+')) result = new Plus(result, Parse1());
+                if (Find('-')) result = new Minus(result, Parse1());
+                return result;
+            }
+        }
+
+        public Operation Parse1()
+        {
+            Operation result = Parse2();
+
+            while (true)
+            {
+                if (Find('*')) result = new Multiply(result, Parse2());
+                else if (Find('/')) result = new Divide(result, Parse2());
+                else return result;
+            }
+        }
+
+        public Operation Parse2()
+        {
+            Operation result = null;
+
+            if (Find('-')) result = new NegativeNumbers(Parse2());
+            else if (Find('('))
+            {
+                result = Parse0();
+                if (!Find(')')) Console.WriteLine("Пропущена ')'.");
+
+            }
+            else
+            {
+                double num = 0;
+                int _first_digit = index;
+
+                //При нахождении одной цифры, с помощью Substring() находится полная длина числа
+                while (index < str.Length && (char.IsDigit(str[index]) || str[index] == '.')) index++;
+                try { num = double.Parse(str.Substring(_first_digit, index - _first_digit)); }
+                catch { Console.WriteLine("Ошибка."); }
+                result = new Numbers(num);
+            }
+            return result;
+        }
+
+    }
+}
---
+++ ConsoleApplication1/Operation.cs
@@ -0,0 +1,108 @@
+﻿//Здесь реализуется обработка данных из класса Expression.
+
+namespace Calculator
+{
+    //Абстрактный класс, содержащий метод для вычислений на каждом шаге.
+    public abstract class Operation
+    {
+        public abstract double Calc();
+    }
+
+    public class Numbers : Operation
+    {
+        public double number;
+        public Numbers(double num)
+        {
+            number = num;
+        }
+        public override double Calc()
+        {
+            return number;
+        }
+    }
+
+    public class NegativeNumbers : Operation
+    {
+        public Operation minus;
+        public NegativeNumbers(Operation num)
+        {
+            minus = num;
+        }
+        public override double Calc()
+        {
+            return -minus.Calc();
+        }
+    }
+    // Абстрактный класс для операторов.
+    abstract class Operator : Operation
+    {
+        protected Operation left, right;
+
+        public Operator(Operation l, Operation r)
+        {
+            left = l;
+            right = r;
+        }
+    }
+
+    class Plus : Operator
+    {
+        public Plus(Operation l, Operation r)
+            : base(l, r)
+        {
+
+        }
+
+        public override double Calc()
+        {
+            return left.Calc() + right.Calc();
+        }
+    }
+
+    class Minus : Operator
+    {
+        public Minus(Operation l, Operation r)
+            : base(l, r)
+        {
+
+        }
+
+        public override double Calc()
+        {
+            return left.Calc() - right.Calc();
+        }
+    }
+
+    class Multiply : Operator
+    {
+        public Multiply(Operation l, Operation r)
+            : base(l, r)
+        {
+
+        }
+
+        public override double Calc()
+        {
+            return left.Calc() * right.Calc();
+        }
+    }
+
+    class Divide : Operator
+    {
+        public Divide(Operation l, Operation r)
+            : base(l, r)
+        {
+
+        }
+
+        public override double Calc()
+        {
+            if (right.Calc() == 0)
+            {
+                System.Console.WriteLine("Ошибка! У нас тут не теория пределов, где-то в знаменателе ноль");
+                return 0;
+            }
+            else return left.Calc() / right.Calc();
+        }
+    }
+}
