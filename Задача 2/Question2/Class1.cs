6246c3acac2ddfa45bab855427bd41b2d7473b3f
NikitaYurjevich <80631700+NikitaYurjevich@users.noreply.github.com>
2021-03-30 17:41:54 +0500

    commit

---
+++ Задача 2/Question2/Class1.cs
@@ -0,0 +1,87 @@
+﻿/*
+ * Пояснение:
+ *   Данный класс предназначен для создания списков различных типов. Класс содержит свойства и методы интерфейса IList<T>.
+ *   
+ *   Управление списком осуществляется с помощью полей:
+ *      1) Count - возвращает количество элементов в списке.
+ *      2) this[index] - возвращает элемент списка по указанному индексу.
+ *      
+ *   И методов:
+ *      1) Add(T newItem) - добавляет элемент в конец списка.
+ *      2) Clear() - полностью очищает список.
+ *      3) Contains(T element) - возвращает true, если элемент содержится в списке, в противном случае false.
+ *      4) RemoveAt(int index) - удаляет элемент по указанному индексу. 
+ *      5) Insert(int index, T item) - добавляет элемент в список по указанному индексу.
+ *      6) IndexOf(T element) - возвращает индекс указанного элемента. 
+ */
+
+using System;
+
+namespace ConsoleApplication1
+{
+    public class MyList<T>
+    {
+
+        T[] array;
+        public int Count { get { return array.Length; } set { value = array.Length; } }
+        public T this[int index] { get { return array[index]; } set { array[index] = value; } }
+
+        public MyList()
+        {
+            array = new T[0];
+        }
+
+        public void Add(T newItem)
+        {
+            Array.Resize(ref array, array.Length + 1);
+            array[array.Length - 1] = newItem;
+        }
+
+        public void Clear()
+        {
+            Array.Resize(ref array, 0);
+        }
+
+        public bool Contains(T element)
+        {
+            foreach (T item in array)
+                if (item.Equals(element)) return true;
+
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index >= 0 && index < array.Length)
+            {
+                for (int i = index; i < array.Length - 1; i++)
+                    array[i] = array[i + 1];
+
+                Array.Resize(ref array, array.Length - 1);
+            }
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index >= 0 && index <= array.Length)
+            {
+                Array.Resize(ref array, array.Length + 1);
+
+                for (int i = array.Length - 1; i > index; i--)
+                    array[i] = array[i - 1];
+
+                array[index] = item;
+            }
+        }
+
+        public int IndexOf(T element)
+        {
+            for (int i = 0; i < array.Length; i++)
+                if (array[i].Equals(element)) return i;
+
+            return -1;
+
+        }
+
+    }
+}
---
+++ Задача 2/Question2/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * В методе Main демонстрируется работа класса MyList<T>.
+ */
+
+using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int> int_list = new MyList<int>();
+            Console.WriteLine("Создание нового списка типа int с помощью класса MyList:");
+            Console.WriteLine();
+            int_list.Add(8);
+            int_list.Add(4);
+            int_list.Add(80);
+
+             Console.Write("Список после добавления трех элементов: ");
+            for(int i = 0; i<int_list.Count; i++)
+                Console.Write(int_list[i] + " ");
+            Console.WriteLine();
+
+            Console.WriteLine("Проверка, есть ли в списке число 70: " + int_list.Contains(70));
+            Console.WriteLine("Проверка, есть ли в списке число 80: " + int_list.Contains(80));
+
+            Console.WriteLine("Количество элементов в массиве: " + int_list.Count);
+
+            Console.WriteLine("Элемент с индексом [1] до удаления элемента: "  + int_list[1]);
+            int_list.RemoveAt(1);
+            Console.WriteLine("Элемент с индексом [1] после удаления элемента:" + int_list[1]);
+            Console.WriteLine("Количество элементов в массиве: " + int_list.Count);
+
+            int_list.Insert(1, 45);
+            Console.WriteLine("Элемент с индексом [1] после добавления элемента: " + int_list[1]);
+
+            Console.WriteLine("Индекс элемента в списке со значением 8: [{0}]" ,int_list.IndexOf(8));
+
+            Console.Write("Список после проведенных операций: ");
+            for (int i = 0; i < int_list.Count; i++)
+                Console.Write(int_list[i] + " ");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            MyList<string> str_list = new MyList<string>();
+            Console.WriteLine("Создание нового списка типа string с помощью класса MyList:");
+            Console.WriteLine();
+            str_list.Add("How");
+            str_list.Add("are");
+            str_list.Add("you");
+            str_list.Add("?");
+
+            Console.Write("Список после добавления четырех элементов: ");
+            for (int i = 0; i < str_list.Count; i++)
+                Console.Write(str_list[i] + " ");
+            Console.WriteLine();
+
+            Console.WriteLine("Проверка, есть ли в списке строка Where: " + str_list.Contains("Where"));
+            Console.WriteLine("Проверка, есть ли в списке строка How: " + str_list.Contains("How"));
+
+            Console.WriteLine("Количество элементов в массиве: " + str_list.Count);
+
+            Console.WriteLine("Элемент с индексом [0] до удаления элемента: " + str_list[0]);
+            str_list.RemoveAt(0);
+            Console.WriteLine("Элемент с индексом [0] после удаления элемента:" + str_list[0]);
+            Console.WriteLine("Количество элементов в списке: " + str_list.Count);
+
+            str_list.Insert(0, "Where");
+            Console.WriteLine("Элемент с индексом [0] после добавления элемента: " + str_list[0]);
+
+            Console.WriteLine("Индекс строки you: [{0}]", str_list.IndexOf("you"));
+
+            Console.Write("Список после проведенных операций: ");
+            for (int i = 0; i < str_list.Count; i++)
+                Console.Write(str_list[i] + " ");
+            Console.WriteLine();        
+        }
+    }
+}
